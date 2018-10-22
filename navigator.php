d4ad1f8ff5850a52e321b90111be042e77ec941c
milagroscm <milagroscruzmamani@gmail.com>
2018-10-22 18:52:58 -0500

    navegador

---
+++ navigator.php
@@ -0,0 +1,83 @@
+<nav class="menu">
+                            <ul class="nav metismenu" id="sidebar-menu">
+                                <li class="active">
+                                    <a href="administrador.php"> <i class="fa fa-home"></i> Home Admin </a>
+                                </li>
+                                 <li>
+                                   
+                                 <li>
+                                    <a href=""> <i class="fa fa-male"></i> Proceso de Admision <i class="fa arrow"></i> </a>
+                                    <ul>
+                                        <li> <a href="Procesos.php">
+                                    Administrar Conductores
+                                </a> </li>
+                                        <li> <a href="nuevoProceso.php">
+                                    Agregar Conductor
+                                </a> </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href=""> <i class="fa fa-male"></i> Seleccionar <i class="fa arrow"></i> </a>
+                                    <ul>
+                                        <li> <a href="Personas.php">
+                                    Administrar Conductores
+                                </a> </li>
+                                        <li> <a href="nuevaPersona.php">
+                                    Agregar Conductor
+                                </a> </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href=""> <i class="fa fa-dashboard"></i> Visualizar <i class="fa arrow"></i> </a>
+                                    <ul>
+                                        <li> <a href="Autos.php">
+                                    Administrar Autos
+                                </a> </li>
+                                        <li> <a href="nuevoAuto.php">
+                                    Agregar Auto
+                                </a> </li>
+                                    </ul>
+                                </li>
+                                 <li>
+                                    <a href=""> <i class="fa fa-male"></i> Buscar <i class="fa arrow"></i> </a>
+                                    <ul>
+                                        <li> <a href="Docentes.php">
+                                    Docente Participante
+                                </a> </li>
+                                        <li> <a href="Administrativo.php">
+                                    Administrativo Participante
+                                </a> </li>
+                                <li> <a href="Alumnos.php">
+                                    Alumno Participante
+                                </a> </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href=""> <i class="fa fa-users"></i> Operadores <i class="fa arrow"></i> </a>
+                                    <ul>
+                                        <li> <a href="Usuarios.php">
+                                    Administrar Usuarios
+                                </a> </li>
+                                        <li> <a href="NuevoUsuario.php">
+                                    Agregar Usuario
+                                </a> </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href=""> <i class="fa fa-male"></i> Personal <i class="fa arrow"></i> </a>
+                                    <ul>
+                                        <li> <a href="Personas.php">
+                                    Administrar Conductores
+                                </a> </li>
+                                        <li> <a href="nuevaPersona.php">
+                                    Agregar Conductor
+                                </a> </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="Aplicacion_Ustandar.php"> <i class="fa fa-map-marker"></i> Aplicacion </a>
+                                </li>
+                                
+            
+                            </ul>
+                        </nav>
